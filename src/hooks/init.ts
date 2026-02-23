0bced2234f68572c6c50a33c6c6cf74e9b3e7c83
Nick Christensen <nick.g.christensen@gmail.com>
2026-02-23 14:36:15 -0600

    Fix lint errors

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -1,8 +1,9 @@
 import {Hook} from '@oclif/core'
 
 const hook: Hook.Init = async function (options) {
-  if (['-v', '--version'].includes(process.argv[2])) {
+  if (['--version', '-v'].includes(process.argv[2])) {
     this.log(`v${options.config.version}`)
+    // eslint-disable-next-line n/no-process-exit, unicorn/no-process-exit
     process.exit(0)
   }
 }
